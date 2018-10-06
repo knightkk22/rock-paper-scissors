1e77c5b9723d3a760648ae107b60e2e0249f126d
knightkk22 <43912807+knightkk22@users.noreply.github.com>
2018-10-06 11:43:17 -0500

    Update rockpaperscissors.py

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -3,9 +3,11 @@ import random
 print("WELCOME")
 print("PLAY ROCK PAPER SCISSORS")
 print("rock=0, paper=1, scissors=2")
+
 playerselection = input("Choose 0,1,2")
 playerselection = int(playerselection) # cast as integer
 computerselection = random.randint(0,2)
+
 if computerselection == 0 and playerselection == 0:
   print("Computer throws rock")
   print("Game ends in tie")
